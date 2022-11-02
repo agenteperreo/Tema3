68352fd7a60a89c3f5588e55e7625a4868cdbf2d
igarcia <igarcar@iesnervion.es>
2022-11-02 13:02:30 +0100

    02/11/2022

--- Tema3/src/bucles_dowhile/Ejercicio3.java
+++ Tema3/src/bucles_dowhile/Ejercicio3.java
@@ -1,9 +1,26 @@
 package bucles_dowhile;
 
+import java.util.Scanner;
+
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+		//Declaramos las variables
+		int num, suma=0, numeros=0;
+		
+		//Abrimos el escaner
+		Scanner sc = new Scanner(System.in);
+		
+		//Pedimos el numero al usuario
+		System.out.print("Dime un numero: ");
+		num=sc.nextInt();
+		
+		do {
+			numeros++;
+			suma+=numeros;
+		}while(numeros<num);
 
 	}
 
