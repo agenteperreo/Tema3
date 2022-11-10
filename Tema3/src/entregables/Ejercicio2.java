ad3bcc48b27efd25bfd526f0bb90e049d41735df
igarcia <igarcar@iesnervion.es>
2022-11-10 14:42:20 +0100

    10/11/2022

---
+++ Tema3/src/entregables/Ejercicio2.java
@@ -0,0 +1,25 @@
+package entregables;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+
+		//Declaración de variables
+		int num, cont=0;
+		
+		//Abrimos el escaner
+		Scanner sc = new Scanner(System.in);
+		
+		//Pedimos el numero
+		System.out.print("Dime un numero: ");
+		num=sc.nextInt();
+		
+		for(int i=num; i<1; i--) {
+			
+		}
+
+	}
+
+}
